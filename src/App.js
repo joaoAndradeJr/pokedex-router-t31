b0de17b93c0c1db90a294e8218731f7b5fc174a6
Joao Andrade Jr <fullservicemanaus@gmail.com>
2023-05-15 15:19:59 -0300

    feat: req 01

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
 import pokemonList from './data';
 import Pokedex from './components/Pokedex';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
-      <Pokedex pokemonList={ pokemonList } />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={ (props) => <Pokedex { ...props } pokemonList={ pokemonList } /> }
+        />
+        <Route path="/about" component={ About } />
+      </Switch>
     </div>
   );
 }
